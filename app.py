8a0f81fc1af5572da30488625257d0bd225d0d03
Rthaweewat <thaweewat.data@gmail.com>
2022-07-20 23:30:51 +0700

    add py and pdf

---
+++ app.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import pdfplumber
+import re
+import numpy as np
+from thefuzz import fuzz
+from thefuzz import process
+
+# Place PDF path here
+PATH = "/genius_pdf.pdf"
+
+with pdfplumber.open(PATH) as pdf:
+	pages = pdf.pages
+	page_no = []
+	text_page = []
+	chap = []
+	for i,pg in enumerate(pages):
+		# Get page no and text
+		page = pdf.pages[i]
+		text = page.extract_text()
+		# Fix space
+		text = text.replace("\r", " ")
+		text = text.replace("\n", " ")
+		text = text.replace("\t", " ")
+		# Fix multiple spaces
+		text = " ".join(text.split())
+		# Get real page
+		get_chap = re.findall(r'บทที่ (\d+)', text)
+		# append lists
+		chap.append(get_chap)
+		text_page.append(text)
+		page_no.append(i)
+
+# Combine lists
+df = pd.DataFrame(list(zip(page_no, chap, text_page, )),
+                  columns =['Page No', 'chapter', 'Text'])
+# Get Clean chapter no.
+df['chapter'] = df['chapter'].str[0]
+df['chapter'] = df['chapter'].fillna(method='ffill')
+df['chapter'] = np.where(df.index < 21, 0, df['chapter'])
+
+df_chap_only = df.query('chapter != 0')
+df_chap_only['Score'] = df_chap_only.apply(lambda row : get_ratio(row['Text'], target_word), axis = 1)
+df_chap_only[['Page No','chapter','Text', 'Score']].sort_values('Score', ascending=False)
+
