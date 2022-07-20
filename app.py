d03aca5da5be0d6b0ea75954b51b6c92dd7f2387
Rthaweewat <thaweewat.data@gmail.com>
2022-07-21 00:00:11 +0700

    add download button

--- app.py
+++ app.py
@@ -9,30 +9,30 @@ from thefuzz import fuzz
 PATH = "genius_pdf.pdf"
 
 with pdfplumber.open(PATH) as pdf:
-	pages = pdf.pages
-	page_no = []
-	text_page = []
-	chap = []
-	for i, pg in enumerate(pages):
-		# Get page no and text
-		page = pdf.pages[i]
-		text = page.extract_text()
-		# Fix space
-		text = text.replace("\r", " ")
-		text = text.replace("\n", " ")
-		text = text.replace("\t", " ")
-		# Fix multiple spaces
-		text = " ".join(text.split())
-		# Get real page
-		get_chap = re.findall(r"บทที่ (\d+)", text)
-		# append lists
-		chap.append(get_chap)
-		text_page.append(text)
-		page_no.append(i)
+    pages = pdf.pages
+    page_no = []
+    text_page = []
+    chap = []
+    for i, pg in enumerate(pages):
+        # Get page no and text
+        page = pdf.pages[i]
+        text = page.extract_text()
+        # Fix space
+        text = text.replace("\r", " ")
+        text = text.replace("\n", " ")
+        text = text.replace("\t", " ")
+        # Fix multiple spaces
+        text = " ".join(text.split())
+        # Get real page
+        get_chap = re.findall(r"บทที่ (\d+)", text)
+        # append lists
+        chap.append(get_chap)
+        text_page.append(text)
+        page_no.append(i)
 
 # Combine lists
 df = pd.DataFrame(
-	list(zip(page_no, chap, text_page, )), columns=["Page No", "chapter", "Text"]
+    list(zip(page_no, chap, text_page,)), columns=["Page No", "chapter", "Text"]
 )
 # Get Clean chapter no.
 df["chapter"] = df["chapter"].str[0]
@@ -41,17 +41,29 @@ df["chapter"] = np.where(df.index < 21, 0, df["chapter"])
 
 
 def get_ratio(word, target):
-	return fuzz.token_set_ratio(word, target)
+    return fuzz.token_set_ratio(word, target)
 
 
-target_word = st.text_input('ใส่ข้อความที่นี่:', ' ')
+target_word = st.text_input("ใส่ข้อความที่นี่:", " ")
 
 df_chap_only = df.query("chapter != 0")
 df_chap_only["Score"] = df_chap_only.apply(
-	lambda row: get_ratio(row["Text"], target_word), axis=1
+    lambda row: get_ratio(row["Text"], target_word), axis=1
 )
 final = df_chap_only[["Page No", "chapter", "Text", "Score"]].sort_values(
-	"Score", ascending=False
+    "Score", ascending=False
 )
 
-st.dataframe(data=final[['chapter','Text','Score']].head(5), width=None, height=None)
+st.dataframe(data=final[["chapter", "Text", "Score"]].head(5), width=None, height=None)
+
+
+@st.cache
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode("utf-8")
+
+
+csv = convert_df(final.head(10))
+st.download_button(
+    label="ดาวโหลดผลการค้นหา (CSV)", data=csv, file_name="large_df.csv", mime="text/csv",
+)
